235301b84012fdb4c9aa0f85e0c052d9e78ee215
zhbanova nadezhda <nadezhda381706-2@yandex.ru>
2018-12-27 15:11:57 +0300

    add new stack class

--- samples/sample_performance_check.cpp
+++ samples/sample_performance_check.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "time.h"
+#include "tnewstack.h"
 
 using namespace std;
 
 int main()
 {
+  char * mem = new char[100];
+  TNewStack<char>  ns(100,mem);
   cout << "Dummy sample\n";
   return 0;
 }
