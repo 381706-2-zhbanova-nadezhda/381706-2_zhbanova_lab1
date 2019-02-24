5ed22b72ce4166bff67745019b42ee5c3e14d6ab
zhbanova nadezhda <nadezhda381706-2@yandex.ru>
2019-02-24 20:22:49 +0300

    try to fix travis failures

--- include/tpolynomial.h
+++ include/tpolynomial.h
@@ -150,7 +150,9 @@ TPolynomial &TPolynomial::operator+=(TMonomial &m)
 
     if ( last_el != NULL )
     {
-      last_el -> SetNext( new TMonomial(m) );
+      TMonomial *tmp = new TMonomial(m);
+      last_el -> SetNext( tmp );
+      //last_el -> SetNext( new TMonomial(m) );
       k++;
     }
   }
@@ -186,8 +188,7 @@ TPolynomial &TPolynomial::operator-=(TMonomial &m)
 
     if ( last_el != NULL )
     {
-      TMonomial *tmp = new TMonomial(m);
-      last_el -> SetNext(tmp);
+      last_el -> SetNext( new TMonomial(m) );
       last_el -> SetCoeff( last_el ->GetCoeff() * (-1) );
       k++;
     }
